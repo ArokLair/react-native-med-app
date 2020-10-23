8fce5efe23feb9f416a970e9accc47b9659b1021
ArokLair <luismiguel.ulloa@gmail.com>
2020-10-23 10:05:47 -0500

    Sample

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Start Here and Test GitHub</Text>
+      <Text>Start Here and Test GitHub not login</Text>
       <StatusBar style="auto" />
     </View>
   );

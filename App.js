8ec096faa34c222fa87f52dff6072da454a1fbc0
ArokLair <luismiguel.ulloa@gmail.com>
2020-10-23 11:58:58 -0500

    Navigator

--- App.js
+++ App.js
@@ -1,22 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import AppNavigator from './navigation/AppNavigator';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Start Here and Test GitHub</Text>
-      <Text>Aqui hay difrencias entre commit y push</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <AppNavigator/>
+    );
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;

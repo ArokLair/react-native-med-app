1f9bda7eb9f67913fb0c7d3ba0c540a5f82e34ac
ArokLair <luismiguel.ulloa@gmail.com>
2020-10-23 10:07:00 -0500

    Commit

--- App.js
+++ App.js
@@ -5,7 +5,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Start Here and Test GitHub not login</Text>
+      <Text>Start Here and Test GitHub</Text>
+      <Text>Aqui hay difrencias entre commit y push</Text>
       <StatusBar style="auto" />
     </View>
   );
